bd60c16f7be17deedbab732118c163afc977f62e
marcosDiFilippo <marcosjesusdifilippo@gmail.com>
2025-10-02 08:52:39 -0300

    update 4
    Cambios en el modo light, sobre mi, contacto e inicio

--- script/script.js
+++ script/script.js
@@ -1,98 +1,156 @@
+class BurguerMenu {
+    #openClosed;
+    #nav;
+    #navLink;
+
+    constructor (openClosed, nav, navLink) {
+        this.#openClosed = openClosed;
+        this.#nav = nav;
+        this.#navLink = navLink;
+    }
+
+    openClosedBurguerMenu() {
+        openClosed.addEventListener("change", () => {
+            nav.classList.toggle("openClosed");
+        });
+    }
+    
+    verificateMenuOpen() {
+        navLink.forEach(link => {
+            link.addEventListener("click", () => {
+                nav.classList.remove("openClosed");
+                openClosed.checked = false;
+            });
+        });
+    }
+}
+class PanelSettings {
+    #liPanel;
+    #panel;
+
+    constructor (liPanel, panel) {
+        this.#liPanel = liPanel;
+        this.#panel = panel;
+    }
+
+    activeteLightMode(button, body, cardsColor, p, titles, languages, header, labelsRadio, slide, pSections, footer, pFooter){
+        this.#liPanel.addEventListener("click", () => {
+            this.#panel.classList.toggle("activo");
+        });
+
+        button.addEventListener("click", () => {
+            body.classList.toggle("light");
+            header.classList.toggle("light");
+            cardsColor.forEach(cardsColor => {
+                cardsColor.classList.toggle("light");
+            })
+            p.forEach(p => {
+                p.classList.toggle("light");
+            })
+            slide.classList.toggle("light");
+            titles.forEach(titles => {
+                titles.classList.toggle("light");
+            })
+            navLink.forEach(navLink => {
+                navLink.classList.toggle("light");
+            })
+            labelsRadio.forEach(labelsRadio => {
+                labelsRadio.classList.toggle("light");
+            })
+            languages.forEach(languages => {
+                languages.classList.toggle("light");
+            })
+            pSections.forEach(pSection => {
+                pSection.classList.toggle("light");
+            })
+            footer.classList.toggle("light");
+            pFooter.classList.toggle("light");
+        });
+    }
+}
+class ProjectSection {
+    #inputsRadio;
+
+    constructor (inputsRadio) {
+        this.#inputsRadio = inputsRadio;
+    }
+
+    slide (arrayStacks, slidea) {
+        arrayStacks[0].style.display = "flex";
+
+        for (let i = 1; i < arrayStacks.length; i++) {
+            arrayStacks[i].style.display = "none";
+        }
+
+        inputsRadio.forEach(inputsRadio => {
+            inputsRadio.addEventListener("click", () => {
+
+                arrayStacks.forEach(arrayStacks => {
+                    arrayStacks.style.display = "none";
+                })
+                
+                const value = inputsRadio.value;
+                
+                if (value == "fullstack") {
+                    arrayStacks[0].style.display = "flex";
+                    slide.style.transform = "translateX(0)";
+                }
+                else if (value == "backend") {
+                    arrayStacks[1].style.display = "flex";
+                    slide.style.transform = "translateX(100%)";
+                }
+                else if (value == "frontend") {
+                    arrayStacks[2].style.display = "flex";
+                    slide.style.transform = "translateX(200%)";
+                }
+            });
+        })
+    }
+}
+//Menu Hamburguesa
 const openClosed = document.querySelector("#burger");
 const nav = document.querySelector("nav");
 const navLink = document.querySelectorAll(".navLink");
 
-openClosed.addEventListener("change", () => {
-    nav.classList.toggle("openClosed");
-});
+const burguerMenu = new BurguerMenu(openClosed, nav, navLink);
 
-navLink.forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("openClosed");
-        openClosed.checked = false;
-    });
-});
+burguerMenu.openClosedBurguerMenu();
+burguerMenu.verificateMenuOpen();
 
+//Panel Settings
 const liPanel = document.querySelector(".liPanel");
 const panel = document.querySelector(".panelSettings");
 
-liPanel.addEventListener("click", () => {
-    panel.classList.toggle("activo");
-});
-
 const button = document.getElementById("theme-checkbox");
-
 const body = document.querySelector("body");
-const section = document.querySelectorAll("section");
 const cardsColor = document.querySelectorAll(".card");
 const p = document.querySelectorAll(".card p");
+const pSections = document.querySelectorAll(".p-sections");
 const titles = document.querySelectorAll("h1, h3, h4"); 
 const languages = document.querySelectorAll(".languages");
 const header =  document.querySelector("header");
 const labelsRadio = document.querySelectorAll(".labels-radio");
 const slide = document.querySelector(".slide");
+const footer = document.querySelector("footer");
+const pFooter = document.querySelector("footer p");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("light");
-    header.classList.toggle("light");
-    cardsColor.forEach(cardsColor => {
-        cardsColor.classList.toggle("light");
-    })
-    p.forEach(p => {
-        p.classList.toggle("light");
-    })
-    slide.classList.toggle("light");
-    titles.forEach(titles => {
-        titles.classList.toggle("light");
-    })
-    navLink.forEach(navLink => {
-        navLink.classList.toggle("light");
-    })
-    labelsRadio.forEach(labelsRadio => {
-        labelsRadio.classList.toggle("light");
-    })
-    languages.forEach(languages => {
-        languages.classList.toggle("light");
-    })
-});
+const panelSettings = new PanelSettings(liPanel, panel);
 
+panelSettings.activeteLightMode(button, body, cardsColor, p, titles, languages, header, labelsRadio, slide, pSections, footer, pFooter);
+
+//Seccion de proyectos
 const inputsRadio = document.querySelectorAll(".inputs-radio");
 
 const fullstack = document.querySelector("#projects-fullstack");
 const backend = document.querySelector("#projects-backend");
 const frontend = document.querySelector("#projects-frontend");
+
 const arrayStacks = [fullstack, backend, frontend];
 
-arrayStacks[0].style.display = "flex";
+const projectSection = new ProjectSection(inputsRadio);
+projectSection.slide(arrayStacks, slide);
 
-for (let i = 1; i < arrayStacks.length; i++) {
-    arrayStacks[i].style.display = "none";
-}
-
-inputsRadio.forEach(inputsRadio => {
-    inputsRadio.addEventListener("click", () => {
-
-        arrayStacks.forEach(arrayStacks => {
-            arrayStacks.style.display = "none";
-        })
-        
-        const value = inputsRadio.value;
-        
-        if (value == "fullstack") {
-            fullstack.style.display = "flex";
-            slide.style.transform = "translateX(0)";
-        }
-        else if (value == "backend") {
-            backend.style.display = "flex";
-            slide.style.transform = "translateX(100%)";
-        }
-        else if (value == "frontend") {
-            frontend.style.display = "flex";
-            slide.style.transform = "translateX(200%)";
-        }
-    });
-})
-
+//Animacion de escritura
 const texts = ["Developer", "Frontend", "Backend"];
 let count = 0;
 let index = 0;
@@ -124,4 +182,6 @@ function type() {
         }
     }
 }
+
+//llama la funcion
 type();
